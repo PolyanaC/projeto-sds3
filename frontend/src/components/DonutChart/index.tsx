17029e1719368f843fa081b27cae534a28568498
Polyana <polyanacsousa@gmail.com>
2021-05-07 20:32:22 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,36 +1,39 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
-import {BASE_URL} from 'utils/requests';
+import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
     labels: string[];
     series: number[];
 }
 const DonutChart = () => {
+    //nomeEstado-FunçãoAlteraEstado-useState-Tipo-ValorInicialDaVariávél     
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    //Forma errada
-    let chartData: ChartData = {labels: [], series: []};
+    useEffect(() => {
+        //Fazendo a chamada do backend
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-    //Fazendo a chamada do backend
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-         .then(response => {
-             const data = response.data as SaleSum[];
-             const myLabels = data.map(x => x.sellerName);
-             const mySeries = data.map(x => x.sum);
-
-             chartData = {
-                            labels: myLabels,
-                            series: mySeries
-                          }
-            console.log(chartData);
-         })
+                setChartData(
+                    {
+                        labels: myLabels,
+                        series: mySeries
+                    }
+                );
+            })
+    }, []);
 
     //const mockData = {
-      //  series: [477138, 499928, 444867, 220426, 473088],
-        //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //  series: [477138, 499928, 444867, 220426, 473088],
+    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
@@ -38,12 +41,12 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options = {{...options, labels: chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
-         />
+        />
     );
 }
 
